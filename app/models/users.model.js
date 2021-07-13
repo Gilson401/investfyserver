139ea44a36f61ad72ce305d0d1ff07be8e58d60b
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2021-07-12 22:17:48 -0300

    local postgre

--- app/models/users.model.js
+++ app/models/users.model.js
@@ -3,12 +3,10 @@ module.exports = (sequelize, Sequelize) => {
         
       email: { type: Sequelize.STRING, allowNull: false   },
       name: { type: Sequelize.STRING, allowNull: false, },
-    //   created_at: {  type: Sequelize.DATE, allowNull: true, },
-    //   updated_at: {  type: Sequelize.DATE,  allowNull: true, },
-      password: {   type: Sequelize.STRING, allowNull: false,  }
+       password: {   type: Sequelize.STRING, allowNull: false,  }
     }
     ,
-    {   // don't add the timestamp attributes (updatedAt, createdAt)
+    {  
         timestamps: false,       
      }
       
